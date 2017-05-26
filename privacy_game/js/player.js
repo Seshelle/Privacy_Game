1b495d2ab844e2175988b5ff235d0b17e8087527
Richard Mao <richmao@gmail.com>
2017-05-25 22:23:33 -0700

    animated intro cutscene

--- privacy_game/js/player.js
+++ privacy_game/js/player.js
@@ -15,7 +15,7 @@ var Player = function (game, x, y, frame) {
 	this.numBullets = 0;
 
 	this.currPowerup = "None";
-	this.powerupText = game.add.text(game.world.width - 400, game.world.height - 48, 'Power Up: None', {fontSize: '32px', fill: '#FFF'});
+	this.powerupText = game.add.text(700, game.world.height + 10, 'Power Up: None', {fontSize: '20px', fill: '#000'});
 
 	this.fireKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	this.PUKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
@@ -39,63 +39,65 @@ Player.prototype.update = function() {
 	this.body.angularVelocity = 0;
 	
 	//go towards mouse pointer when mouse button down
-	if (game.input.activePointer.isDown)
-	{
-		//only start moving after a fifth of a second had passed
-		//makes it easier to spin without interfering with movement and vice versa
-		downTime += game.time.elapsed;
-		//if (downTime > 200){
-			//accelerate player toward pointer location
-			accelerateToPoint(this, playerMass * acceleration)
-		//}
-		/*else{
-			//rotate charracter when swiping across screen, equal to speed of swipe
-			if (touchY + game.camera.y < this.y){
-				this.body.angularForce += (touchX - game.input.activePointer.position.x) * -playerMass;
-			}
-			else{
-				this.body.angularForce += (touchX - game.input.activePointer.position.x) * playerMass;
-			}
+	if(!start){
+		if (game.input.activePointer.isDown)
+		{
+			//only start moving after a fifth of a second had passed
+			//makes it easier to spin without interfering with movement and vice versa
+			downTime += game.time.elapsed;
+			//if (downTime > 200){
+				//accelerate player toward pointer location
+				accelerateToPoint(this, playerMass * acceleration)
+			//}
+			/*else{
+				//rotate charracter when swiping across screen, equal to speed of swipe
+				if (touchY + game.camera.y < this.y){
+					this.body.angularForce += (touchX - game.input.activePointer.position.x) * -playerMass;
+				}
+				else{
+					this.body.angularForce += (touchX - game.input.activePointer.position.x) * playerMass;
+				}
+				
+				if(touchX + game.camera.x > this.x){
+					this.body.angularForce += (touchY - game.input.activePointer.position.y) * -playerMass;
+				}
+				else{
+					this.body.angularForce += (touchY - game.input.activePointer.position.y) * playerMass;
+				}
+			}*/
 			
-			if(touchX + game.camera.x > this.x){
-				this.body.angularForce += (touchY - game.input.activePointer.position.y) * -playerMass;
-			}
-			else{
-				this.body.angularForce += (touchY - game.input.activePointer.position.y) * playerMass;
-			}
-		}*/
+		}else{
+			//player comes to a smooth stop when mouse button is let go
+			player.body.velocity.x *= 0.9;
+			player.body.velocity.y *= 0.9;
+			//set cursor downtime to zero since it was released
+			downTime = 0;
+		}
 		
-	}else{
-		//player comes to a smooth stop when mouse button is let go
-		player.body.velocity.x *= 0.9;
-		player.body.velocity.y *= 0.9;
-		//set cursor downtime to zero since it was released
-		downTime = 0;
-	}
-	
-	touchX = game.input.activePointer.position.x;
-	touchY = game.input.activePointer.position.y;
-	
-	//adjust for world coordinates, as camera coordinates won't necessarily match
-	var actualPointerY = game.input.activePointer.position.y + game.camera.y;
-	var actualPointerX = game.input.activePointer.position.x + game.camera.x;
-	
-	//get angle between pointer and character and accelerate in that direction
-    var angle = Math.atan2(actualPointerY - this.y, actualPointerX - this.x);
-	this.body.rotation = angle + game.math.degToRad(90);
+		touchX = game.input.activePointer.position.x;
+		touchY = game.input.activePointer.position.y;
+		
+		//adjust for world coordinates, as camera coordinates won't necessarily match
+		var actualPointerY = game.input.activePointer.position.y + game.camera.y;
+		var actualPointerX = game.input.activePointer.position.x + game.camera.x;
+		
+		//get angle between pointer and character and accelerate in that direction
+	    var angle = Math.atan2(actualPointerY - this.y, actualPointerX - this.x);
+		this.body.rotation = angle + game.math.degToRad(90);
 
-	if(this.fireKey.justPressed()){
-		this.fire();
-	}
+		if(this.fireKey.justPressed()){
+			this.fire();
+		}
 
-	if(this.PUKey.justPressed()){
-		//use powerup
-		usePU(this.currPowerup);
-		this.currPowerup = "None";
-		this.powerupText.text = 'Power Up: ' + this.currPowerup;
-	}
+		if(this.PUKey.justPressed()){
+			//use powerup
+			usePU(this.currPowerup);
+			this.currPowerup = "None";
+			this.powerupText.text = 'Power Up: ' + this.currPowerup;
+		}
 
-	this.body.onBeginContact.add(collect, this);
+		this.body.onBeginContact.add(collect, this);
+	}
 };
 
 Player.prototype.fire = function(){
